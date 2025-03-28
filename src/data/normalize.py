8ca9b66d32adae18a56e01538a30962f2ee60107
Rubenchek <rubenchek@gmail.com>
2025-03-28 19:32:24 +0700

    Création de datasets normalisés

--- src/data/normalize.py
+++ src/data/normalize.py
@@ -26,7 +26,7 @@ def main():
     # Sauvegarde des datasets normalisés
     pd.DataFrame(X_train_scaled, columns=X_train.columns).to_csv(os.path.join(output_folderpath, 'X_train_scaled.csv'), index=False)
     pd.DataFrame(X_test_scaled, columns=X_test.columns).to_csv(os.path.join(output_folderpath, 'X_test_scaled.csv'), index=False)
-    
+
     logger.info('Normalization complete')
 
 if __name__ == '__main__':
